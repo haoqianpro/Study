45e362b902ea75cb4a7e25a2f3169b4d12592f60
haoqianpro <18109692046@163.com>
2019-05-15 21:57:31 +0800

    2019-5-15-2

--- MySQL/创建表/orders.sql
+++
@@ -1,6 +0,0 @@
-create table orders (
-	order_num int not null auto_increment,
-	order_data datetime not null,
-	cust_id int not null,
-	primary key(order_num)
-);
--- MySQL/视图/cust_vend_view.sql
+++
@@ -1,2 +0,0 @@
-create view cust_vend as
-select customers.cust_id , vendors.vend_id from vendors , customers;
